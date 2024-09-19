e1351d3d965684421f915717357091c2c1d6af79
Domains18 <gibsonsgibson88@gmail.com>
2024-09-19 22:43:16 +0300

    hardware configurations nix

--- config/home.nix
+++ config/home.nix
@@ -1,7 +1,7 @@
 {pkgs ...}: let 
-    username = "kemboi";
+    username = "Kemboi";
     in {
-        imports = [,.common];
+        imports = [,.packages];
         fonts.fontsconfig.enable = true;
 
         xdgg = {
@@ -13,5 +13,99 @@
         };
 
 
-        home
+        home = {
+            username= "${username}";
+            homeDirectory = "/home/${username}";
+            stateVersion = "23.11";
+            packages = with pkgs; [
+      vim
+      wget
+      w3m
+      dmenu
+      neofetch
+      neovim
+      starship
+      bat
+      bazecor
+      cargo
+      celluloid
+      chatterino2
+      clang-tools_9
+      dunst
+      efibootmgr
+      elinks
+      eww
+      feh
+      flameshot
+      flatpak
+      floorp
+      fontconfig
+      freetype
+      fuse-common
+      gcc
+      gimp
+      git
+      github-desktop
+      gnome.gnome-keyring
+      gnugrep
+      gnumake
+      gparted
+      gnugrep
+      grub2
+      hugo
+      kitty
+      libverto
+      luarocks
+      lxappearance
+      mangohud
+      neovim
+      nfs-utils
+      ninja
+      nodejs
+      nomacs
+      openssl
+      os-prober
+      nerdfonts
+      pavucontrol
+      picom
+      polkit_gnome
+      powershell
+      protonup-ng
+      python3Full
+      python.pkgs.pip
+      qemu
+      ripgrep
+      rofi
+      steam
+      steam-run
+      sxhkd
+      st
+      stdenv
+      synergy
+      swaycons
+      terminus-nerdfont
+      tldr
+      trash-cli
+      unzip
+      variety
+      virt-manager
+      xclip
+      xdg-desktop-portal-gtk
+      xfce.thunar
+      xorg.libX11
+      xorg.libX11.dev
+      xorg.libxcb
+      xorg.libXft
+      xorg.libXinerama
+      xorg.xinit
+      xorg.xinput
+      zoxide
+      (lutris.override {
+        extraPkgs = pkgs: [
+          # List package dependencies here
+          wineWowPackages.stable
+          winetricks
+        ];
+      })
+        };
     }
